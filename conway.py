c01b79646b3690ff24ef3e04d3639d72896ca622
willsawyer2609 <wmsawyer2609@gmail.com>
2022-04-01 14:16:26 +1000

    implement four GoL rules

--- conway.py
+++ conway.py
@@ -45,14 +45,40 @@ class GameOfLife:
         - Any live cell with more than three live neighbors dies, as if by overpopulation.
         - Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction
         '''
-        #get weighted sum of neighbors
+        #get weighted sum of neighbours
         #PART A & E CODE HERE
+        grid = self.getGrid()
+        N = grid.shape[0]
+        weightingGrid = np.zeros((N, N), np.int)
+
+        for i in range(N):
+            rowMin = max(i - 1, 0)
+            rowMax = min(i + 2, N)
+            for j in range(N):
+                colMin = max(j - 1, 0)
+                colMax = min(j + 2, N)
+                neighborhood = grid.copy()[rowMin:rowMax, colMin:colMax]
+                neighborhood[i - rowMin, j - colMin] = 0
+                weightingGrid[i, j] = np.sum(neighborhood)
         
-        #implement the GoL rules by thresholding the weights
-        #PART A CODE HERE
+        # implement the GoL rules by thresholding the weights
+        evolvedGrid = np.zeros((N, N), np.int)
+
+        for i in range(N):
+            for j in range(N):
+                if grid[i, j] == 0: # currently dead
+                    if weightingGrid[i, j] == 3: # reproduction
+                        evolvedGrid[i, j] = 1
+                else: # currently alive
+                    if weightingGrid[i, j] < 2: # underpopulation
+                        evolvedGrid[i, j]= 0
+                    elif weightingGrid[i, j] > 3: # overpopulation
+                        evolvedGrid[i, j]= 0
+                    else: # survival
+                        evolvedGrid[i, j]= 1
         
-        #update the grid
-#        self.grid = #UNCOMMENT THIS WITH YOUR UPDATED GRID
+        # update the grid
+        self.grid = evolvedGrid
     
     def insertBlinker(self, index=(0,0)):
         '''

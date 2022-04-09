083c6b5dde28ee402ff5b4c023986fcc832fe115
willsawyer2609 <wmsawyer2609@gmail.com>
2022-04-09 14:49:06 +1000

    Add new files for parts e and h

---
+++ e-convolved_2.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Game of life script with animated evolution
+
+Created on Tue Jan 15 12:37:52 2019
+
+@author: shakes
+"""
+import conway
+
+padding = 10
+
+#read plain text file
+with open("e-pattern_2.txt", "r") as text_file:
+        txtString = text_file.read()
+
+#create the game of life object
+life = conway.GameOfLife(N=1024, fastMode=True)
+life.insertFromPlainText(txtString, padding)
+cells = life.getStates() #initial state
+
+#-------------------------------
+#plot cells
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig = plt.figure()
+
+plt.gray()
+
+img = plt.imshow(cells, animated=True)
+
+def animate(i):
+    """perform animation step"""
+    global life
+    
+    life.evolve()
+    cellsUpdated = life.getStates()
+    
+    img.set_array(cellsUpdated)
+    
+    return img,
+
+interval = 50 #ms
+
+#animate 24 frames with interval between them calling animate function at each frame
+ani = animation.FuncAnimation(fig, animate, frames=24, interval=interval, blit=True)
+#~ animate(0)
+
+plt.show()
